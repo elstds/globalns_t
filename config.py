09e8f8f8dc8b0f2dd0142578cd9555cead40979d
excretory <elstxds@proton.me>
2025-01-31 13:41:18 +0500

    Added database

--- config.py
+++ config.py
@@ -1,5 +1,8 @@
 import os
 
+base_dir = os.path.abspath(os.path.dirname(__file__))
+
+
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'default_key'
     API_LINK = 'https://order.drcash.sh/v1/order'
@@ -7,3 +10,4 @@ class Config:
         'Content-Type': 'application/json',
         'Authorization': 'Bearer RLPUUOQAMIKSAB2PSGUECA'
     }
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///' + os.path.join(base_dir, 'app.db')
